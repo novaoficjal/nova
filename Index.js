5b221b644aa300801c4efc8fab68b8675501c123
novaoficjal <55056221+novaoficjal@users.noreply.github.com>
2019-09-08 16:55:04 +0200

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,25 @@
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const config = require('./config.json');
+const client = new SteamUser();
+
+
+const logOnOptions = {
+	accountName: config.username,
+	password: config.password,
+	twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+	console.log('succesfully logged on.');
+	client.setPersona(SteamUser.Steam.EPersonaState.Online);
+	client.gamesPlayed(["Custom Game",440]);
+});
+
+client.on("friendMessage", function(steamID, message) {
+	if (message == "hi") {
+		client.chatMessage(steamID, "hello, this works.");
+	}
+});
